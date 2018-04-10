7deb0ab2d8d40e815d820d9951fa6d168d466dea
Jose <joseeduardovillanuevaferrer@gmail.com>
2018-04-09 21:06:20 -0500

    interfaz terminada

--- app/src/main/java/com/elvilla/personas/Principal.java
+++ app/src/main/java/com/elvilla/personas/Principal.java
@@ -1,13 +1,39 @@
 package com.elvilla.personas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
-
+    private ListView lv;
+    private String [] opc;
+    private Intent in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        lv = findViewById(R.id.lstOpciones);
+        opc = getResources().getStringArray(R.array.opciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_gallery_item,opc);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        in = new Intent(Principal.this,Registro.class);
+                        startActivity(in);
+                        break;
+                    case 1:
+                        in = new Intent(Principal.this,Listado.class);
+                        startActivity(in);
+                        break;
+                }
+            }
+        });
     }
 }
